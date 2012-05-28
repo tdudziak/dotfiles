ce27866fc961e28b139e61703f64bd8f8ad04796
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2012-05-29 01:31:31 +0200

    Updated X-related stuff.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,6 +7,8 @@ import XMonad.Layout.Spiral
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ToggleLayouts
 
+import Graphics.X11.ExtraTypes.XF86
+
 import qualified Data.Map as Map
 
 myManageHook = composeAll (
@@ -19,7 +21,11 @@ myManageHook = composeAll (
 myKeys config = Map.fromList [
           ((mod1Mask, xK_F4), kill)
         , ((mod1Mask, xK_grave), sendMessage ToggleLayout)
-        , ((mod1Mask .|. shiftMask, xK_t), spawn "trac report/2")
+        --, ((mod1Mask .|. shiftMask, xK_t), spawn "trac report/2")
+        , ((mod1Mask .|. shiftMask, xK_m), spawn "disper --cycle-stages=-s:-S -C")
+        , ((0, xF86XK_HomePage), spawn "$BROWSER")
+        , ((0, xF86XK_Launch5), spawn "gvim")
+        , ((0, xF86XK_Launch9), spawn "gnome-terminal --command=ipython")
     ]
 
 myLayout =
@@ -29,7 +35,6 @@ myLayout =
     $ mainLayout ||| Mirror mainLayout
  where
     mainLayout = Tall 1 (3/100) (1/2)
-    -- mainLayout = spiral (0.62)
     
 
 alterConfig config = config {
