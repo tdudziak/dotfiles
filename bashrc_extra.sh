5069ae221f4b7d9d195237dec0ce1c47bcc4c0f5
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2019-02-04 18:24:58 +0100

    Fix Bash line editing and easier support for custom tmux pane title in aliases

--- bashrc_extra.sh
+++ bashrc_extra.sh
@@ -1,18 +1,26 @@
 # prompt and tmux pane title {{{
+function __update_tmux_pane {
+    pane_title=""
+    if [ -n "TMUX_PANE" ]; then
+        [ -n "$PROJECT" ] && pane_title="[$PROJECT]:"
+        pane_title+="$PWD"
+        [ -n "$1" ] && pane_title+=" ($1)"
+        printf '\033]2;%s\033\\' "$pane_title"
+    fi
+}
+
 function __prompt_command {
     if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
         # color support
-        c_project="$(tput setaf 3)"
-        c_reset="$(tput sgr0)"
+        c_project="\[$(tput setaf 3)\]"
+        c_reset="\[$(tput sgr0)\]"
     fi
     if [ -n "$PROJECT" ]; then
         PS1="${c_project}[$PROJECT]${c_reset}:\w\$ "
-        pane_title="[$PROJECT]:$PWD"
     else
         PS1="\u@\h:\w\$ "
-        pane_title="$PWD"
     fi
-    [ -n "TMUX_PANE" ] && printf '\033]2;%s\033\\' "$pane_title"
+    __update_tmux_pane
 }
 PROMPT_COMMAND=__prompt_command
 # }}}
